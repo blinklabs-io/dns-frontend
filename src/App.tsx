761c338a587e158e8c13dca1dde38216ebebb91f
JaeBrian <bnlee419@gmail.com>
2025-11-01 18:05:36 -0700

    set up basic frontend
    Signed-off-by: JaeBrian <bnlee419@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-50">
+      <div className="text-4xl sm:text-5xl md:text-6xl font-semibold tracking-widest uppercase text-slate-900">
+        dns-frontend
+      </div>
+    </div>
+  )
+}
