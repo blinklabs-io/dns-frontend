2d3cce61df2354dedc780d0c765c3863731f0bf1
Jaekook Brian Lee <107831698+JaeBrian@users.noreply.github.com>
2025-12-07 09:00:47 -0800

    Wallet connector (#14)
    Signed-off-by: JaeBrian <bnlee419@gmail.com>
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>
    Co-authored-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useRef } from 'react'
+import CardanoWalletConnector from './components/WalletConnector'
+import type { WalletConnectorRef } from './components/WalletConnector'
+import { NetworkType } from '@cardano-foundation/cardano-connect-with-wallet-core'
+
 export default function App() {
+  const walletRef = useRef<WalletConnectorRef>(null)
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-50">
-      <div className="text-4xl sm:text-5xl md:text-6xl font-semibold tracking-widest uppercase text-slate-900">
-        dns-frontend
+    <div className="min-h-screen flex flex-col items-center justify-start bg-linear-to-br from-black to-gray-900">
+      
+      <div className="w-full max-w-md px-2 mt-10 ml-auto">
+        <CardanoWalletConnector
+          ref={walletRef}
+          variant="default"
+          listLayout="dropdown"
+          networkType={NetworkType.TESTNET}
+        />
       </div>
     </div>
   )
